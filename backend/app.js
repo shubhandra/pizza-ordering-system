50b7b63d7797c28dd1e0f60aa0bed86c88edaf14
rar <rar@gmail.com>
2020-04-17 19:10:03 -0400

    Placing order by user

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,20 @@ const express = require('express')
 require('dotenv').config()
 const port = process.env.PORT
 const userRouter = require('./routers/userRouter')
-require('./db/db')
+
+require('./db/db');
+
+//Import Routes Here
+const routes     = require('./routers/order');
 
 const app = express()
 
 app.use(express.json())
 app.use(userRouter)
+
+
+//routes  start from here..
+app.use('/api/order/',routes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
