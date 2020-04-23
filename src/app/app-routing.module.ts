8da5f972bb2ff464d32ec610f9cfd43cc3d556c7
Smita21 <62159384+Smita21@users.noreply.github.com>
2020-04-23 23:11:27 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
-
+import { PizzaListComponent } from './components/pizza-list/pizza-list.component';
+import { PizzaComponent } from './components/pizza-list/pizza/pizza.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -14,6 +15,8 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'admin-dashboard', component: AdminDashboardComponent },
   { path: 'user-dashboard', component: UserDashboardComponent },
+  { path: 'pizzas', component: PizzaListComponent }
+
 ];
 
 @NgModule({
